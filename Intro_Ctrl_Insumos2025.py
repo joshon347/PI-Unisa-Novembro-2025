3b9e09c6e06a710ba0d3593b64e340f74141e877
joshon347 <84936982+joshon347@users.noreply.github.com>
2025-11-30 17:13:53 -0300

    Add files via upload

---
+++ Intro_Ctrl_Insumos2025.py
@@ -0,0 +1,20 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> >>> def cadastrar_insumos():
+... ...     produto = input ("produto: ")
+... ...     Codigo = input ("Codigo: ")
+... ...     Qtd.append({"produto":produto, "codigo":codigo})
+... ...     print("produto cadatrado!")
+... ... def lista_de_produtos():
+... ...     for a in produtos:
+... ...         print(f"{a['produto']}-{a['codigo']}")
+... ...
+... >>>     while True:
+...         opção = input ("1-cadastrar| 2-listar | 3-executar:")
+...         if opcao=='1'
+...         cadastrar_produto()
+...        elif opcao =='2':
+...        listar_produto
+...        elif opcao =='3':
+...       break
+...       else
